c321d321641470a99f1ee68e22b59b1609e5469d
Willian Wiegand <willian.wiegand@gmail.com>
2022-08-20 21:27:40 -0300

    first

---
+++ Curso/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Curso
+{
+    class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
---
+++ Curso/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1, p2;
+            p1 = new Pessoa();
+            p2 = new Pessoa();
+
+            Console.WriteLine("Dados da Primeira Pessoa:");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da Segunda Pessoa:");
+            Console.Write("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine("Pessoal Mais Velha: "+p1.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoal Mais Velha" + p2.Nome);
+            }
+        }
+    }
+}
